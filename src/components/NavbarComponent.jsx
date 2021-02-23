60ebcb41b6563e80c00f2ace7114dbfaf357a1f9
jcleow <jc.leow94@gmail.com>
2021-02-23 17:07:14 +0800

    Shifted sign in button the right

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import {
+  Modal, Button, Navbar, Nav,
+} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import LoginForm from './LoginForm.jsx';
 import { getInfoFromCookie } from '../helper.js';
 
 export default function NavbarComponent() {
   const [loggedInUsername, setLoggedInUsername] = useState(null);
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // To run this everytime to check if user is logged in
   useEffect(() => {
     const currUsername = getInfoFromCookie();
     if (currUsername) {
@@ -22,6 +29,7 @@ export default function NavbarComponent() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
+
             <LinkContainer to="/home">
               <Nav.Link> HomePage</Nav.Link>
             </LinkContainer>
@@ -31,16 +39,33 @@ export default function NavbarComponent() {
             <LinkContainer to="/viewAll">
               <Nav.Link>View All</Nav.Link>
             </LinkContainer>
+          </Nav>
+          <Nav className="ml-auto">
             {loggedInUsername
               ? <Nav.Link>{loggedInUsername}</Nav.Link>
               : (
-                <LinkContainer to="/login">
-                  <Nav.Link>Login</Nav.Link>
-                </LinkContainer>
+                <>
+                  <Nav.Link onClick={handleShow}>Sign in</Nav.Link>
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <LoginForm />
+                    </Modal.Body>
+                  </Modal>
+                </>
               )}
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>
     </>
   );
 }
+
+// (
+//   <LinkContainer to="/login">
+//     <Nav.Link>Login</Nav.Link>
+//   </LinkContainer>
+// )}
