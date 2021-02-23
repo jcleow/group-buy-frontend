e02041e84b7b5c8494e4ebad7606b32b66f44917
jeremylim91 <jeremylim_91@live.com>
2021-02-24 00:29:48 +0800

    added main payments pge, purchase summary pg, payment instructions pg

---
+++ src/components/Payments/MainPaymentPage.jsx
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import NavbarComponent from '../NavbarComponent.jsx';
+import PurchaseSummary from './PurchaseSummary.jsx';
+
+const PAGE_NAMES = {
+  PURCHASE_SUMMARY: 'see purchase summary',
+  PAYMENT_INSTRUCTIONS: 'see payment instructions',
+  UPLOAD_RECEIPT: 'see upload receipt',
+  CONFIRMATION_OF_RECEIPT: 'see confirmation of receipt',
+};
+const {
+  PURCHASE_SUMMARY,
+  PAYMENT_INSTRUCTIONS,
+  UPLOAD_RECEIPT,
+  CONFIRMATION_OF_RECEIPT,
+} = PAGE_NAMES;
+
+// const PURCHASE_SUMMARY = 'see purchase summary';
+// const PAYMENT_INSTRUCTIONS = 'see payment instructions';
+// const UPLOAD_RECEIPT = 'see upload receipt';
+// const CONFIRMATION_OF_RECEIPT = 'see confirmation of receipt';
+export default function MainPaymentPage() {
+  const [mode, setMode] = useState(PURCHASE_SUMMARY);
+
+  const managePageDisplay = () => {
+    switch (mode) {
+      case PURCHASE_SUMMARY:
+        <PurchaseSummary setMode={setMode} PAGE_NAMES={PAGE_NAMES} />;
+        break;
+      case PAYMENT_INSTRUCTIONS:
+        <PaymentInstructions />;
+        break;
+      case UPLOAD_RECEIPT:
+        <UploadReceipt />;
+        break;
+      case CONFIRMATION_OF_RECEIPT:
+        <ConfirmationOfReceipt />;
+        break;
+
+      default:
+        <PurchaseSummary />;
+    }
+  };
+  return (
+    <div>
+      <NavbarComponent />
+      <div className="container page-container" />
+      {managePageDisplay}
+    </div>
+  );
+}
---
+++ src/components/Payments/PurchaseSummary.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+
+export default function PurchaseSummary({setMode, PAGE_NAMES}) {
+  const handleBtnClick = () => {
+    // save the state to the cookie
+    // updateMode to switch to next page
+    setMode(PAGE_NAMES.PAYMENT_INSTRUCTIONS);
+  };
+
+  return (
+    <>
+      <div className="row">
+        <div className="col summary">
+          {/* insert the summary of item purchased, qty, price per unit, and total price */}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col terms-n-conditions">
+          <div>
+            By proceeding, you agree that you have read, and abide Group-Buys
+            terms and conditions.
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <Button className="btn btn-primary" onClick={handleBtnClick}>
+            Next
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+PurchaseSummary.propTypes = {
+  setMode: PropTypes.func.isRequired,
+  PAGE_NAMES: PropTypes.objectOf.isRequired,
+};
