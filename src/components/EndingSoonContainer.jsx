07af1cbb8b9fbadf02559238244f368612f7773c
Veena-S <veenasivan@gmail.com>
2021-02-25 07:44:31 +0800

    Added label for progress bar

--- src/components/EndingSoonContainer.jsx
+++ src/components/EndingSoonContainer.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useContext, useEffect } from 'react';
 import moment from 'moment';
 import { GroupBuyContext, sortListingsByEndDateAction } from '../store.jsx';
-import './EndingSoonContainer.css';
+import ListCardWithImage from './ListCardWithImage.jsx';
 
 export default function EndingSoonContainer() {
   const { store, dispatch } = useContext(GroupBuyContext);
@@ -25,21 +23,7 @@ export default function EndingSoonContainer() {
       </div>
       <div className="row listings-card-row flex-nowrap">
         {listings.map((singleListing) => (
-          <div className="col-6 col-md-3 ml-auto">
-            <figure className="figure">
-              <img src={singleListing.images.img1} className="figure-img img-fluid ending-soon-image" alt="..." />
-              <div id="img-overlay">
-                <span id="time-left-number">{moment(new Date(singleListing.endDate)).fromNow(true)}</span>
-                {' '}
-                left
-              </div>
-              <figcaption className="figure-caption text-dark font-weight-bolder">{singleListing.title}</figcaption>
-            </figure>
-            <div className="progress">
-              {/* <div id="order-progress" className="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" />
-              <label htmlFor="order-progress">Ordered so far</label> */}
-            </div>
-          </div>
+          <ListCardWithImage singleListing={singleListing} />
         ))}
       </div>
     </div>
