14c6943f9dd71ded98e518c12a470db3eae96898
jeremylim91 <jeremylim_91@live.com>
2021-02-24 18:36:49 +0800

    create form to accept uploads

--- src/components/Payments/UploadReceipt.jsx
+++ src/components/Payments/UploadReceipt.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
 
 export default function UploadReceipt({ setMode, PAGE_NAMES }) {
+  const [selectedFile, setSelectedFile] = useState(null);
   const handleBtnClick = () => {
     // save the state to the cookie
     // updateMode to switch to next page
     setMode(PAGE_NAMES.CONFIRMATION_OF_RECEIPT);
   };
 
+  const handleFileSelection = (e) => {
+    console.log('uploading file');
+    console.log(e.target.files);
+    setSelectedFile(e.target.files[0]);
+    // e.target.files is an array; file is in index 0
+  };
+
+  const handleFileUpload = () => {
+    const data = new FormData();
+    data.append('file', selectedFile);
+  };
   return (
     <>
       <div className="row">
@@ -17,10 +29,26 @@ export default function UploadReceipt({ setMode, PAGE_NAMES }) {
         </div>
       </div>
       <div className="row">
-        <div className="col terms-n-conditions">
-          <div>
-            Photo upload
-          </div>
+        <div className="col">
+
+          <form method="post" action="#" id="#">
+            <div className="form-group files">
+              <label>Upload Your File </label>
+              <input type="file" name="receiptImg" className="form-control" onChange={(e) => handleFileSelection(e)} />
+            </div>
+          </form>
+          <Button type="button" className="btn btn-secorndary" onClick={handleFileUpload}>
+            Upload
+          </Button>
+          {/* <form>
+            <h3>React File Upload</h3>
+            <div className="form-group">
+              <input type="file" />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary" type="submit">Upload</button>
+            </div>
+          </form> */}
         </div>
       </div>
       <div className="row">
