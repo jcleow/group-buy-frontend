90786b728cee2f2a9eefe88f52b6027951c65fa2
jeremylim91 <jeremylim_91@live.com>
2021-02-28 07:48:27 +0800

    started on code to connect payment w detailed listing

--- src/components/Payments/MainPaymentPage.jsx
+++ src/components/Payments/MainPaymentPage.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { writeStorage, useLocalStorage } from '@rehooks/local-storage';
+import React, { useState, useEffect, useContext } from 'react';
+import { useLocalStorage } from '@rehooks/local-storage';
 import PurchaseSummary from './PurchaseSummary.jsx';
 import ConfirmationOfReceipt from './ConfirmationOfReceipt.jsx';
 import UploadReceipt from './UploadReceipt.jsx';
 import PaymentInstructions from './PaymentInstructions.jsx';
 import PAGE_NAMES from '../utility/paymentPageNames.js';
+import { GroupBuyContext } from '../../store.jsx';
 
 const {
   PURCHASE_SUMMARY,
@@ -14,6 +15,14 @@ const {
 } = PAGE_NAMES;
 
 export default function MainPaymentPage() {
+  const { store } = useContext(GroupBuyContext);
+  const { totalQuantityOrdered, selectedListingData } = store;
+
+  console.log('selectedListingData is:');
+  console.log(selectedListingData);
+  console.log('totalQuantityOrdered is:');
+  console.log(totalQuantityOrdered);
+
   const [mode, setMode] = useState(PURCHASE_SUMMARY);
 
   const [currPage] = useLocalStorage('mode');
