2a1822d3fdafd5438bd7c3cb5e04e499a962a14d
jcleow <jc.leow94@gmail.com>
2021-03-03 21:19:11 +0800

    Separate activity graph and  purchasers table into separate files, shifted all campaign progress related helpers into utility folder

---
+++ src/components/CampaignProgress/ActivityChart.jsx
@@ -0,0 +1,104 @@
+import React, { useState, useEffect, useContext } from 'react';
+import {
+  FlexibleXYPlot,
+  YAxis,
+  XAxis,
+  Hint,
+  LineMarkSeries,
+  GradientDefs,
+  AreaSeries,
+  LineSeries,
+
+} from 'react-vis';
+import 'react-vis/dist/style.css';
+import { generatePastSevenDays, getLowestYVal, generateYAxisTickValues } from '../utility/campaignProgressHelper.jsx';
+import {
+  loadCurrListingPurchases, CampaignProgressProvider, CampaignProgressContext, updatePurchaseDateDelivered,
+} from '../../campaignProgressStore.jsx';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+export default function ActivityChart() {
+  const [value, setValue] = useState(null);
+  const { campaignStore, dispatchCampaign } = useContext(CampaignProgressContext);
+  useEffect(() => {
+    // pass in dispatch fn and currListingId
+    loadCurrListingPurchases(dispatchCampaign, 3);
+  }, []);
+
+  // Handler for hovering away from curr data point
+  const forgetValue = () => {
+    setValue(null);
+  };
+
+  // Handler for hovering into another data point
+  const rememberValue = (val) => {
+    setValue(val);
+  };
+
+  const xAxisTickValues = generatePastSevenDays();
+  const yAxisTickValues = generateYAxisTickValues(campaignStore);
+  const datesAndPurchasesCount = campaignStore.pastSevenDaysCount;
+  const YLOW = getLowestYVal(datesAndPurchasesCount);
+
+  // Calc total purchases
+  const calcTotalPurchases = () => {
+    const totalPurchases = campaignStore.allPurchases.reduce((acc, curr) => acc + curr.quantity, 0);
+    return totalPurchases;
+  };
+  return (
+    <div>
+      <h4 className="mt-2 ml-5">
+        Total Purchases:
+        {calcTotalPurchases()}
+      </h4>
+      <h6 className="mt-2 ml-5">Daily Purchase Count (7 Days)</h6>
+      <div className="d-flex justify-content-center activity-graph">
+        <FlexibleXYPlot onMouseLeave={() => { setValue(null); }} xType="ordinal">
+          <YAxis tickValues={yAxisTickValues} />
+          <XAxis tickValues={xAxisTickValues} />
+          <LineMarkSeries
+            onNearestX={rememberValue}
+            onValueMouseOut={forgetValue}
+            color="#F37B36"
+            data={datesAndPurchasesCount}
+            size={0}
+          />
+          <GradientDefs>
+            <linearGradient id="CoolGradient" x1="0" x2="0" y1="0" y2="1">
+              <stop offset="0%" stopColor="#F37B36" stopOpacity={0.4} />
+              <stop offset="100%" stopColor="#FAF9FF" stopOpacity={0.1} />
+            </linearGradient>
+          </GradientDefs>
+          <AreaSeries
+            color="url(#CoolGradient)"
+            data={datesAndPurchasesCount}
+          />
+          {value ? (
+            <LineSeries
+              data={[{ x: value.x, y: value.y }, { x: value.x, y: YLOW }]}
+              stroke="#f5f6fa"
+              size={1}
+            />
+          ) : null}
+          {value
+          && (
+          <Hint value={value}>
+            <div className="rv-hint_content" style={{ background: 'rgba(255, 255, 255, 0.5)', color: 'rgba(0,0,0, 0.6)' }}>
+              <p>
+                Date:
+                {' '}
+                <b>{value.x}</b>
+              </p>
+              <p>
+                Total Purchases:
+                {' '}
+                <b>{value.y}</b>
+              </p>
+            </div>
+          </Hint>
+          )}
+        </FlexibleXYPlot>
+      </div>
+    </div>
+  );
+}
--- src/components/utility/campaignProgressHelper.jsx
+++ src/components/utility/campaignProgressHelper.jsx
@@ -87,3 +87,56 @@ export const generatePaginationOptions = (indexAllPurchases) => {
   };
   return paginationOptions;
 };
+
+// Helper that generates yAxis Tick Values
+export const generateYAxisTickValues = (campaignStore) => {
+  const individualPurchaseCount = campaignStore.pastSevenDaysCount.map((dayCount) => dayCount.y);
+
+  const maxVal = Math.max(...individualPurchaseCount);
+  const minVal = Math.min(...individualPurchaseCount);
+  const purchaseCountRange = maxVal - minVal;
+
+  const yAxisInterval = Math.floor(purchaseCountRange / 5);
+  const yAxisTickValues = [];
+  // To ensure that second highest value is not too close to the top axis label
+  for (let i = 0; i < purchaseCountRange - yAxisInterval; i += yAxisInterval) {
+    yAxisTickValues.push(i);
+  }
+  // To ensure top of the graph always has the largest (label) value
+  yAxisTickValues.push(maxVal);
+
+  return yAxisTickValues;
+};
+
+// Helper that generates and pushes the past 7 days
+export const generatePastSevenDays = () => {
+  const pastSevenDaysArray = [];
+  for (let i = 7; i >= 0; i -= 1) {
+    // Start from today's date then progressive go backwards
+    const singleDate = new Date();
+    singleDate.setDate(singleDate.getDate() - i);
+
+    // Format into DD/MM
+    const options = { day: '2-digit', month: '2-digit' };
+    const formattedDate = singleDate.toLocaleDateString('en-GB', options);
+
+    // Append into an accumulative array
+    pastSevenDaysArray.push(formattedDate);
+  }
+  return pastSevenDaysArray;
+};
+
+// Helper that gets the lowest y-value
+export const getLowestYVal = (purchaseRange) => {
+  let yLow = null;
+  purchaseRange.forEach((day) => {
+    if (day.y < yLow || yLow === null) {
+      yLow = day.y;
+    }
+  });
+
+  if (yLow !== null) {
+    return yLow;
+  }
+  return 0;
+};
